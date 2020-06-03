af42acbf0325a28c1c66946a2acb5ea42b3b4e6b
Alessandro Pezzè <igougi.ui@gmail.com>
2020-06-03 20:27:34 +0200

    docs: update staging URL

--- scripts/add_baseurl_firebase.sh
+++ scripts/add_baseurl_firebase.sh
@@ -3,5 +3,5 @@
 # It is intended to document how to update the configuration of Firebase
 
 # Adds BASE_URL to FireBase config
-firebase functions:config:set network.base_url="https://pokeapi-test-b6137.firebaseapp.com" --project "<PROJECT_ID>"
+firebase functions:config:set network.base_url="https://staging.pokeapi.co" --project "<PROJECT_ID>"
 firebase functions:config:set network.base_url="https://pokeapi.co" --project "<PROJECT_ID>"
